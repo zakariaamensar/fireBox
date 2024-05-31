fc24560187abbb5f22c4229f7825f9e927214aaf
zakaria amensar <44840345+zakariaamensar@users.noreply.github.com>
2024-05-31 19:31:53 +0100

    Add files via upload

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,80 @@
+import React, { useContext, useState, useEffect } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendPasswordResetEmail,
+  onAuthStateChanged,
+  updateEmail as firebaseUpdateEmail,
+  updatePassword as firebaseUpdatePassword,
+} from "firebase/auth";
+import { auth } from "../firebase";
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [currentLink, setCurrentLink] = useState("/");
+
+  function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function logout() {
+    return signOut(auth);
+  }
+
+  function resetPassword(email) {
+    return sendPasswordResetEmail(auth, email);
+  }
+
+  function updateEmail(email) {
+    if (currentUser) {
+      return firebaseUpdateEmail(currentUser, email);
+    } else {
+      return Promise.reject(new Error("No current user"));
+    }
+  }
+
+  function updatePassword(password) {
+    if (currentUser) {
+      return firebaseUpdatePassword(currentUser, password);
+    } else {
+      return Promise.reject(new Error("No current user"));
+    }
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signup,
+    login,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
